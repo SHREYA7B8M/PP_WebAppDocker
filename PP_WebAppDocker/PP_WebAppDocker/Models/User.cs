60b73e496d037b19067559abbbbed4e4d1ef2295
s66829020@gmail.com <s66829020@gmail.com>
2023-09-22 15:40:00 +0530

    first commit

---
+++ PP_WebAppDocker/PP_WebAppDocker/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace PP_WebAppDocker.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
